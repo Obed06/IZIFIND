36ff585f54383e4c10fd78d2d88b35f3659c0de3
Obed06 <leonardovodouhe06@outlook.fr>
2023-11-20 20:47:23 +0100

    || add 20 nov. 2023 -- 20:47 ||

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from .views import (
 	RegisterUserView,
 	login, sign_up,
+	generate_password_and_send_email,
 )
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'), # api inscription
+    path('password/reset/', generate_password_and_send_email, name='password_reset'), # api mot de passe oublier
 
     # TESTS
     path('test_connect/', login, name="test_connect"),
