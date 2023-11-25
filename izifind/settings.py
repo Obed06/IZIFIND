89132a3e0af82c0a678ca33b70d6382b2072256b
Obed06 <leonardovodouhe06@outlook.fr>
2023-11-25 22:08:27 +0100

    || add 25 nov. 2023 -- 22:07 || Migrated DATABASE TO Postgresql

--- izifind/settings.py
+++ izifind/settings.py
@@ -99,10 +99,22 @@ WSGI_APPLICATION = 'izifind.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#      'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'izifind',
+        'USER': 'solv',
+        'PASSWORD': '@Chekvolonte',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
